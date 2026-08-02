fad3ff8fc628e2e14c0ff8de111ac1b1a64bf7df
Sarha <148410263+Sarhadactyl@users.noreply.github.com>
2026-08-02 03:26:07 -0700

    Update UNO tournament data

--- data/uno_bracket.js
+++ data/uno_bracket.js
@@ -4,7 +4,7 @@ window.UNO_BRACKET_DATA = {
     "name": "UNO Tournament 2.0",
     "signup_deadline_timestamp": 1785524400
   },
-  "generated_at_timestamp": 1785666222,
+  "generated_at_timestamp": 1785666366,
   "guild_id": "995954559044304896",
   "guild_name": "Modded Among Us Lobbies",
   "matches": [
@@ -27,12 +27,12 @@ window.UNO_BRACKET_DATA = {
       "created_at_timestamp": 1785635786,
       "created_by_id": "972228046323064903",
       "deadline_timestamp": 1785722186,
-      "finalized": false,
+      "finalized": true,
       "game_id": 3,
       "guild_id": "995954559044304896",
       "player_one_id": "906424513137377280",
       "player_two_id": "1072462136443404379",
-      "winner_id": null
+      "winner_id": "906424513137377280"
     },
     {
       "channel_id": "1533292519461228747",
